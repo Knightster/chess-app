000370cbc1b589d211e1d1b30c7e05036bb8bda0
emestabillo <emsestabillo@gmail.com>
2017-07-11 18:28:19 -0400

    Edited Heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'chess-app-knightster.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'knightster.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
