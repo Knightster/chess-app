72e1fd33880381963bb3d9c80909f1b94359f367
emestabillo <emsestabillo@gmail.com>
2017-07-18 21:56:40 -0400

    Created piece model

---
+++ db/migrate/20170719014442_create_pieces.rb
@@ -0,0 +1,15 @@
+class CreatePieces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pieces do |t|
+      t.string :type 
+      t.string :color
+      t.integer :x_position
+      t.integer :y_position
+      t.integer :game_id
+      t.integer :user_id 
+      t.boolean :captured
+
+      t.timestamps
+    end
+  end
+end
