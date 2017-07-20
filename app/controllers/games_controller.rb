bbc55ae7cbbf611c7f47aefea6d1c1501f082c51
4Chazz <chazzbrooks@gmail.com>
2017-07-20 17:24:37 +0000

    Added Game Model, Chg. Game Controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+# before_action :authenticate_user!, only: [:new, :create]
 
   def index
 
@@ -9,11 +10,21 @@ class GamesController < ApplicationController
   end
 
   def create
+    @game = Game.create(game_params)
+    @game.white_player = current_user
 
   end
 
   def show
-    
+
   end
 
+
+  private
+
+  def game_params
+    params.require(:game).permit(:white_player_id, :black_player_id, :game_name)
+
+
 end
+
