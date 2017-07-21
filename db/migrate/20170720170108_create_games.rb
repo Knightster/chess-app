e8cd14184f0ec872008a1351fdc7d8a34011b2aa
emestabillo <emsestabillo@gmail.com>
2017-07-20 23:02:06 -0400

    Added columns to create games db

--- db/migrate/20170720170108_create_games.rb
+++ db/migrate/20170720170108_create_games.rb
@@ -1,7 +1,12 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-
+      t.string :status
+      t.integer :white_player_id
+      t.integer :black_player_id
+      t.integer :turn
+      t.integer :winner_id
+      
       t.timestamps
     end
   end
