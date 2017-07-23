a4499ab3705f600b469711bcdbd2f5f36f35e52d
emestabillo <emsestabillo@gmail.com>
2017-07-23 14:13:08 -0400

    Added is_blocked method to piece class

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,27 @@
 class Piece < ApplicationRecord
+
+
+  #check if horizonal move
+  def horizontal_dir(x_start, x_end, y_start, y_end)
+    if x_start != x_end && y_start == y_end
+      return(x_end - x_start).abs
+    end
+  end
+
+  #check if vertical move
+  def vertical_dir(x_start, x_end, y_start, y_end)
+    if x_start == x_end && y_start != y_end
+      return(y_end - y_start).abs
+    end
+  end
+
+  def diagonal_move
+    if horizontal_dir == vertical_dir
+      return true
+  end
+
+end
+
+def filled_square(x, y)
+    pieces.where(x_dest: x, y_dest: y).last
 end
