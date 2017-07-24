527073e4744f8f633be8975ade542a3a2c7792f4
abdullahalam <abdullah_alam_omi@outlook.com>
2017-07-24 08:24:55 +0000

    Modified game controller and game model of the Knightster

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,25 +1,24 @@
 class GamesController < ApplicationController
   # before_action :authenticate_user!, only: [:new, :create]
 
-  def index; end
+  def index
 
-  def new; end
+  end
+
+  def new
+
+  end
 
   def create
     @game = Game.create(game_params)
     @game.white_player = current_user
-<<<<<<< HEAD
-=======
     @game.status = 'Open'
     @game.update_attributes
   end
 
   def show
 
->>>>>>> 3d1acfc205ac3f6ef1469e68e12f9bbf63273147
   end
-
-  def show; end
 
   private
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,6 @@
 class Game < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :white_player, class_name: 'Player'
   belongs_to :black_player, class_name: 'Player'
-=======
-  belongs_to :white_player, class_name: "User"
-  belongs_to :black_player, class_name: "User", required: false
->>>>>>> 3d1acfc205ac3f6ef1469e68e12f9bbf63273147
   has_many :pieces
 
   def self.needs_second_player?
