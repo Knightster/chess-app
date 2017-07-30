49265fa0f4957dacc4212fff25bcdefd64176dbd
Zayn Rahman <zaynrahn@gmail.com>
2017-07-30 18:45:31 +0000

    Added a valid_move? method to King model

--- app/models/king.rb
+++ app/models/king.rb
@@ -6,4 +6,18 @@ class King < Piece
       '&#9818;'
     end
   end
+
+  def valid_move?(x,y)
+    if horizontal_move?(x_position, x, y_position, y)
+      if delta_x(x_position, x) <= 1
+        !blocked?(x,y)
+      end
+    elsif vertical_move?(x_position, x, y_position, y)
+      if delta_y(y_position, y) <= 1
+        !blocked?(x,y)
+      end
+    elsif diagonal_move?
+      !blocked?(x,y)
+    end  
+  end
 end
