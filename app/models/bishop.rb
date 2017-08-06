d8ef77850d3dc53e2298c721ae2c9f13ac73ed8c
abdullahalam <abdullah_alam_omi@outlook.com>
2017-08-06 09:36:09 +0000

    Add bishop can and can't move diagonlly  method

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -8,5 +8,16 @@ class Bishop < Piece
   end
 
   def valid_move?(x, y)
+    if blocked?(x, y)
+      return false
+    end
+
+    # The bishop can move diagonally
+    if delta_x(x_position, x) == delta_y(y_position, y)
+      return true
+    end
+
+    # The bishop can't move diagonally
+    return false
   end
 end
