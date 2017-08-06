9dea2b54e434b1424c8323799a3f27aa1c0b5f64
abdullahalam <abdullah_alam_omi@outlook.com>
2017-08-06 10:15:26 +0000

    Add queen valid_move? method and queen can move horizonally, vertically, and diagonally

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -8,5 +8,26 @@ class Queen < Piece
   end
 
   def valid_move?(x, y)
+    if blocked?(x, y)
+      return false
+    end
+
+    # The queen can move horizonally
+    if(delta_x(x_position, x) > 0 && delta_y(y_position, y) == 0)
+      return true
+    end
+
+    # The queen can move vertically
+    if(delta_x(x_position, x) == 0 && delta_y(y_position, y) > 0)
+      return true
+    end
+
+    # The queen can move diagonally
+    if(delta_x(x_position, x) == delta_y(y_position, y))
+      return true
+    end
+
+    # The queen can't move horizonally, vertically, and diagonally
+    return false
   end
 end
