0bd1be6dd41b814a0420ed8463ac86afc8b5f5af
abdullahalam <abdullah_alam_omi@outlook.com>
2017-08-06 21:49:37 +0000

    Modified uncomment for the queen valid_move? method

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -13,9 +13,10 @@ class Queen < Piece
     dx = delta_x(x_position, x)
     dy = delta_y(y_position, y)
 
-    return true if dx == dy # The queen can move diagonally
-    return true if dx > 0 && dy.zero?   # The queen can move horizonally
-    return true if dx.zero? && dy > 0   # The queen can move vertically
-    false # The queen can't move horizonally, vertically, and diagonally
+    return true if dx == dy
+    return true if dx > 0 && dy.zero?
+    return true if dx.zero? && dy > 0
+
+    false
   end
 end
