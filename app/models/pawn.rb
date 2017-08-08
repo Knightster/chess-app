fc2007ee514ac57691351743cbdf3b501ec222e9
4Chazz <chazzbrooks@gmail.com>
2017-08-08 18:31:24 +0000

    Working on Pawn Moves

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,4 +6,34 @@ class Pawn < Piece
       '&#9823;'
     end
   end
+
+  def valid_move?(x,y)
+    return false if blocked?(x,y)
+    return false if backwards_move?(y)
+    return true if capture_move?(x,y)
+    return true if delta_y(y_position, y) == 1
+
+  end
+
+
+
+  def backwards_move?(y)
+    color == 'white' ? y_position < y : y_position > y
+  end
+
+  def first_move?(y)
+    if color == 'white' && y_position == 6 || color == 'black' && y_position == 1
+      delta_y(y_position, y) == 2
+  end
+
+
+  def capture_move?(x,y)
+
+  end
+
+  
+
+
+
+
 end
