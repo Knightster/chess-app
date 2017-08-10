8330f63b39ca1c41ee82e8788a449140dccb4338
4Chazz <chazzbrooks@gmail.com>
2017-08-10 14:53:40 +0000

    Added en_passant to Pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -13,7 +13,7 @@ class Pawn < Piece
     return true if capture_move?(x, y)
     return false if horizontal_move?(x)
 
-    right_dist?(y)
+    right_dist?(y) || en_passant?(y)
   end
 
   def backwards_move?(y)
@@ -23,11 +23,13 @@ class Pawn < Piece
   def capture_move?(x, y)
     x_diff = (x_position - x).abs
     y_diff = (y_position - y).abs
-    # captured_piece = square_occupied?(x, y)
+    captured_piece = square_occupied?(x, y)
 
     return true if x_diff == 1 && y_diff == 1
     return false if x_diff > 1 && y_diff > 1
-    return false if square_occupied?(x, y).nil?
+    return false if captured_piece.blank?
+    return x_diff == 1 && y_diff == 0 if captured_piece.state == 'en_passant'
+    false
   end
 
   def horizontal_move?(x)
@@ -43,5 +45,10 @@ class Pawn < Piece
   def first_move?
     return unless (y_position != 1 && color == 'black') || (y_position != 6 && color == 'white')
     (y - y_position).abs == 2 || (y - y_position).abs == 1
+  end
+
+  def en_passant?(y)
+    y_diff = (y_position - y).abs
+    first_move?(y) ? (y_diff == 1 || y_diff == 2) : false
   end
 end
