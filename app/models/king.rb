dbd2af9955715f2d0e2268c2c11bce62bc225e42
abdullahalam <abdullah_alam_omi@outlook.com>
2017-08-11 05:08:47 +0000

    Add the comment of the castle rook on the king side

--- app/models/king.rb
+++ app/models/king.rb
@@ -24,12 +24,13 @@ class King < Piece
     castling_kingside?(x, y) && castling_never_moved?
   end
 
-  # The king can attack to the castle on the kingside
+  # The king can attack to the castle on the king side
   def castling_kingside?(x, y)
     x == 6 && y == y_position && castling_rook_kingside && castling_rook_kingside.castling_never_moved? && \
       castling_rook_kingside.blocked?(4, y_position) == false
   end
 
+  # The rook to castle with king side 
   def castling_rook_kingside
     can_castling_rook_kingside = game.pieces.where(x_position: 0, y_position: y_position, \
                                                    color: 'white' || 'black', type: 'Rook')
