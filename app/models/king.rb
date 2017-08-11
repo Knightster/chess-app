9f08970ea5d6c826053ad65208252dcbfc14c97c
abdullahalam <abdullah_alam_omi@outlook.com>
2017-08-11 04:16:14 +0000

    Add castle model for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -12,6 +12,18 @@ class King < Piece
     return false if blocked?(x, y) # checks if piece is blocking at destination coordinate
     return false if delta_x(x_position, x) > 1
     return false if delta_y(y_position, y) > 1
+
+    # The king can castle
+    if move_castle?(x, y)
+      return true
+    end
+
     true
   end
+
+  # Support castling with the king
+  def move_castle?(x, y)
+    (castling_kingside?(x, y) || castling_queenside?(x, y)) && castling_never_moved?
+  end
+
 end
