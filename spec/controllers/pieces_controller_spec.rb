4900c5d619dbb0d3647ebd98f7e940a2552951a2
emestabillo <emsestabillo@gmail.com>
2017-08-11 21:52:16 -0400

    Added validation before updating attributes

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
+  describe 'pieces#update action' do
+    before :each do
+      @piece.valid_move?
+    end
+    it 'should successfully change piece attribute' do
+      put :update, :id => @piece.id, :piece => {:x_position => x, :y_position => y}      
+    end
+  end
 end
