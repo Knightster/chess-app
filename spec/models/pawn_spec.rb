2d199b954d9aaa8069b1c160a9aae597373d091f
4Chazz <chazzbrooks@gmail.com>
2017-08-21 14:41:14 +0000

    Pawn Spec Tests

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can move vertically' do
+    game = Game.create
+    pawn = Pawn.create(game_id: game.id, x_position: 3, y_position: 1, color: 'white')
+
+    expect(pawn.valid_move?(3, 2)).to be true
+    expect(pawn.valid_move?(3, 3)).to be true
+  end
+
+  it 'can not move two squares afte first move' do
+    game = Game.create
+    pawn = Pawn.create(game_id: game.id, x_position: 3, y_position: 3, color: 'white')
+
+    expect(pawn.valid_move?(3, 5)).to be false
+  end
+
+  it 'can not move horizontally' do
+    game = Game.create
+    pawn = Pawn.create(game_id: game.id, x_position: 2, y_position: 3, color: 'white')
+
+    expect(pawn.valid_move?(3, 3)).to be false
+    expect(pawn.valid_move?(1, 3)).to be false
+  end
 end
